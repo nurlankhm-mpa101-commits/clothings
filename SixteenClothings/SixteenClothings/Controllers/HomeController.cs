7ee3458e1a77c112cfc50a727b025d410e453653
nurlankhm-mpa101-commits <nurlankhm-mpa101@code.edu.az>
2026-01-18 03:07:23 +0400

    update

--- SixteenClothings/SixteenClothings/Controllers/HomeController.cs
+++ SixteenClothings/SixteenClothings/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SixteenClothings.Context;
 using SixteenClothings.Models;
+using SixteenClothings.ViewModels;
 
 namespace SixteenClothings.Controllers;
 
-public class HomeController : Controller
+public class HomeController(AppDbContext _context) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        
+        var products = await _context.Products.Select(x => new ProductGetVM()
+        {
+            Id=x.Id,
+            Name=x.Name,
+            Description=x.Description,
+            ImagePath=x.ImagePath,
+            Price=x.Price,
+            Rating=x.Rating,
+            CategoryName=x.Category.Name
+
+        }).ToListAsync();
+        return View(products);
     }
 }
